eda89e9a7e9e09ea1ab079b8ee54a9773bc22ce6
Bobby <tkdgus1797@gmail.com>
2020-11-02 18:10:50 +0900

    handled main page

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
-
+import {Link} from "react-router-dom";
 
 const Main: React.FC = () => (
     <div>
         <h1>
             This is main page and Type Script is used!
+        </h1>
+        <h1>
+            <Link to="/cases">GO TO CASE LIST</Link>
         </h1>
     </div>
 );
